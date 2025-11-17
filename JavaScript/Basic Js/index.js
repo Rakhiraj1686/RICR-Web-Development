4eef80742273a6aecd1220c2293404c7e2fca800
Rakhiraj1686 <rakhiraj1686@gmail.com>
2025-11-17 19:40:56 +0530

    class work

---
+++ JavaScript/Basic Js/index.js
@@ -0,0 +1,18 @@
+var a = 10;
+
+let b = 20;
+console.log(a, b);
+
+let ar = ["Abc", 1234, 45, 45.67, "a", true];
+console.log(ar);
+
+alert("this is alert");
+
+console.error("ye error hai");
+console.warn("this is warning");
+if (confirm("Are you sure")) {
+  console.log("thanks");
+} else {
+  console.log("ok");
+}
+console.log(45);
