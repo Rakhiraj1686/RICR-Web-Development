5f32d10ea3d6ce3c9125939f4e17753331e794dc
Rakhiraj1686 <rakhiraj1686@gmail.com>
2025-11-21 18:03:44 +0530

    class work

---
+++ JavaScript/PizzzaOrderDelivery/index.js
@@ -0,0 +1,9 @@
+function submit(){
+    console.log("Submit Button Clicked");
+
+    const name= document.getElementById("firstName").value;
+    const lastName= document.getElementById("lastName").value;
+    const flavour = document.getElementById("")
+    console.log(name);
+    console.log(lastName)
+}
