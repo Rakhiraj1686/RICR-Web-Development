b76d4e71da722d75a6483f6a02ac3d13343d2226
Rakhiraj1686 <rakhiraj1686@gmail.com>
2025-11-23 16:24:58 +0530

    calculator

---
+++ JavaScript/calculator/index.js
@@ -0,0 +1,17 @@
+function button(char){
+    if(char === '=') {
+      try{
+        const exp = document.getElementById("display").value;
+        document.getElementById("display").value = eval(exp)
+      }catch(error){
+        alert("Invalid Expression");
+        document.getElementById("display").value=""
+      }
+    }else if(char === 'c') {
+        document.getElementById("display").value=""
+    }else{
+        let exp = document.getElementById("display").value
+        exp = exp + char;
+        document.getElementById("display").value=exp;
+    }
+}
