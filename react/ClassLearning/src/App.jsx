7c35ff10f9efd7389ecc325756acb78f78f7b372
Rakhiraj1686 <rakhiraj1686@gmail.com>
2025-12-31 22:40:57 +0530

    registation

---
+++ react/ClassLearning/src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Register from "./Pages/Register";
+// import Home from ".Pages/Home";
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import { toaster } from "react-hot-toast";
+
+const App = () => {
+  return (
+    <>
+      {/* <BrowserRouter> */}
+        {/* <Toaster position="" reverseOrder={""}/> */}
+        {/* <Routes> */}
+          {/* <Route path="/" element={<Home />} /> */}
+          {/* <Route path="/" element={<Register />} /> */}
+        {/* </Routes> */}
+      {/* </BrowserRouter> */}
+      <Register />
+    </>
+  );
+};
+export default App;
---
+++ react/ClassLearning/src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel culpa rem
+        voluptatibus magnam ab ex eos adipisci tenetur. Eligendi tempora, beatae
+        eaque nulla porro sint sapiente? Nobis nulla cupiditate tenetur!
+      </p>
+    </>
+  );
+};
+export default Home;
