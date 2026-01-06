5bcdfd00ae5226b10595b634b2de2757f781ee3a
Rakhiraj1686 <rakhiraj1686@gmail.com>
2026-01-06 20:38:43 +0530

    firstBackend

---
+++ Node/firstBackend/index.js
@@ -0,0 +1,16 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  console.log("Server is Running....");
+    res.json({message:"Server is running successfully"});
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port,()=>{
+    console.log("Server starter at port ",port); 
+});
