441b7c92754ca6d3072bd83b4a5aa9c0e22804ae
Rakhiraj1686 <rakhiraj1686@gmail.com>
2026-01-10 21:20:03 +0530

    firstBackend

--- Node/firstBackend/src/routers/myRouter.js
+++ Node/firstBackend/src/routers/myRouter.js
@@ -1,10 +1,16 @@
-import express from 'express'
-import { UserRegister,UserLogin,userLogout } from "../controllers/myController.js";
+import express from "express";
+import {
+  UserRegister,
+  UserLogin,
+  userLogout,
+  UserUpdate,
+} from "../controllers/myController.js";
 
 const router = express.Router();
 
-router.post("/register",UserRegister);
-router.post("/login",UserLogin);
-router.get("/logout",userLogout);
+router.post("/register", UserRegister);
+router.post("/login", UserLogin);
+router.get("/logout", userLogout);
+router.put("/update", UserUpdate);
 
 export default router;
